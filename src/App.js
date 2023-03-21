134fc8a7d8eaae910b5d888c89e4736bf09bf23c
Dmitriy K <d.komerzan@gmail.com>
2023-03-21 17:35:20 -0400

    test

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ const App = ({ signOut }) => {
         </Flex>
       ))}
       </View> */}
-      <Button onClick={signOut}>Sign Out (DEV123 )</Button>
+      <Button onClick={signOut}>Sign Out (DEV1 )</Button>
     </View>
   );
 };
